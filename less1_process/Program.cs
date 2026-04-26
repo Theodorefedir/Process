d3dccab281a52f7fa713235ef30403c830f26e88
Fedir Korolov <korolevfeda2008@gmail.com>
2026-04-26 18:49:26 +0200

    Add project files.

---
+++ less1_process/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace less1_process
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region AllProcesses
+            //Process[] processes = Process.GetProcesses();
+            //Console.WriteLine("Process Name \tPID\tPriority\tMachine name\tStart time");
+            //foreach (Process p in processes)
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine($"{p.ProcessName}\t{p.Id}\t{p.BasePriority}" +
+            //       $"\t{p.MachineName}\t{p.StartTime}");
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Red;
+            //        Console.Write(p.ProcessName + " ");
+            //        Console.WriteLine(ex.Message);
+            //        Console.ResetColor();
+            //    }
+            //}
+            #endregion
+
+            #region KillProcess
+
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = @"notepad",
+                Arguments = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\777.txt",
+                WindowStyle = ProcessWindowStyle.Maximized
+            };
+
+            Process pr = Process.Start(info);
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+            pr.Close();//clear reference
+            //pr.CloseMainWindow();
+            //pr.Kill();
+
+            pr.WaitForExit();
+            Console.WriteLine($"ExitCode {pr.ExitCode}");
+            Console.WriteLine($"ExitTime {pr.ExitTime}");
+
+            #endregion
+        }
+    }
+}
---
+++ taskManager/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace taskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Refresh(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = Process.GetProcesses();
+        }
+        private void Button_Kill(object sender, RoutedEventArgs e)
+        {
+            Process select = (Process)grid.SelectedItem;
+            select.Kill();
+        }
+        private void Button_Close(object sender, RoutedEventArgs e)
+        {
+            Process select = (Process)grid.SelectedItem;
+            select.Close();
+        }
+        private void Button_Info(object sender, RoutedEventArgs e)
+        {
+            Process select = (Process)grid.SelectedItem;
+            MessageBox.Show(select.ProcessName);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string path = txtProcessPath.Text;
+            Process.Start(path);
+            MessageBox.Show($"{path} Started");
+            txtProcessPath.Text = "";
+        }
+    }
+}
